8dcbddc0f32bf64684e259ea1796064c0d3f8e69
BySamm <sam.mba.byiringiro@gmail.com>
2024-04-13 20:39:19 +0200

    docstring added

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
     """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+    Generate Pascal's triangle of height n.
+    Pascal's triangle is a mathematical array of binomial
+    coefficients arranged in a
+    triangular form. Each number is the sum of the two directly
+    above it in the previous row.
+    Parameters:
+    - n (int): The number of levels in Pascal's triangle to generate.
+    Returns:
+    - list of lists: A list where each sublist represents a level
+    in Pascal's triangle => If n <= 0, returns an empty
+    The function starts with a base check for non-positive values of n,
+    where it returns an empty list.
+    For positive n, it constructs the triangle level by level.
+    Each level's entries are calculated
+    by adding up appropriate pairs from the previous level.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
